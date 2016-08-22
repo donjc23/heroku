372377ce9230d80f7e66267ea539b6635b2e5630
donjc23 <donwang23@gmail.com>
2016-08-22 15:27:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 var express = require('express');
-var path = require('path');
-
-
 var app = express();
-var port = process.env.PORT;
 
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
-app.set('view engine', 'ejs');
-
-
-app.get('/*', function(req, res){
-  res.render('index.ejs');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(port, function(){
-  console.log("server listening at..port: " + port);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
