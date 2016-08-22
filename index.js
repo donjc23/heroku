d935640cc8d121e7933c408133ed270686e1a7f2
donjc23 <donwang23@gmail.com>
2016-08-22 15:07:52 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="utf-8">
+    <title>d306</title>
+  </head>
+
+  <body>
+     <p>
+      We are d306!
+     </p>      
+    
+  </body>
+</html>
